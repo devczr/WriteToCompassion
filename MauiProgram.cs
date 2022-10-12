231e43adbcfa9bc37abd440793307d040c87b04d
devczr <czr@devczr.com>
2022-10-12 06:31:44 -0600

    Update Colors resource dictionary with m3 color palette based on seed #f4fe9f

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -21,9 +21,7 @@ public static class MauiProgram
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("JosefinSans-Regular.ttf", "Josefin");
-                fonts.AddFont("JosefinSans-SemiBold.ttf", "JosefinSemiBold");
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("JosefinSans-ExtraLight.ttf", "JosefinThin");
             })
             .RegisterAppServices()
             .RegisterViewModels()
